e768b7087bbf9f6dc75c49f7b5e53f811faec24e
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-21 20:23:11 +0330

    Add SquarePacket and TrianglePacket
    Inherited from Packet class.
    Defined SIZE constant in each inherited class.

--- src/main/java/com/blueprinthell/model/Packet.java
+++ src/main/java/com/blueprinthell/model/Packet.java
@@ -3,10 +3,17 @@ package com.blueprinthell.model;
 public abstract class Packet {
 
     private int id;
-    private int size;
     private double speed;
     private double noise;
     private double stateOnWire;
     private Wire currentWire;
     private boolean isAlive = true;
+}
+
+class SquarePacket extends Packet {
+    private final int SIZE = 2;
+}
+
+class TrianglePacket extends Packet {
+    private final int SIZE = 3;
 }
