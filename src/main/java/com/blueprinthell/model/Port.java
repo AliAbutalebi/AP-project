d391867d854a88ebd18b340bc651e6e28ee23bba
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-21 21:35:01 +0330

    Add fields and inherited classes to Port

--- src/main/java/com/blueprinthell/model/Port.java
+++ src/main/java/com/blueprinthell/model/Port.java
@@ -1,4 +1,26 @@
 package com.blueprinthell.model;
 
-public class Port {
+public abstract class Port {
+    private int id;
+    private final boolean isInput;
+    private boolean occupied = false;
+    private Wire connectedWire;
+    private SystemNode parentSystem;
+
+    public Port(boolean isInput, SystemNode parentSystem) {
+        this.isInput = isInput;
+        this.parentSystem = parentSystem;
+    }
+}
+
+class SquarePort extends Port {
+    public SquarePort(boolean isInput, SystemNode parentSystem) {
+        super(isInput, parentSystem);
+    }
+}
+
+class TrianglePort extends Port {
+    public TrianglePort(boolean isInput, SystemNode parentSystem) {
+        super(isInput, parentSystem);
+    }
 }
