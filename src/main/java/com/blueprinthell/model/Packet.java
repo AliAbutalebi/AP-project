2b2ccab1e0560a42898b1df6a6f3bb73e30063ca
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-21 20:10:58 +0330

    Add fields to packet abstract class
    Added id, size, speed, noise, stateOnWire, currentWire, and isAlive field.

--- src/main/java/com/blueprinthell/model/Packet.java
+++ src/main/java/com/blueprinthell/model/Packet.java
@@ -1,4 +1,12 @@
 package com.blueprinthell.model;
 
-public class Packet {
+public abstract class Packet {
+
+    private int id;
+    private int size;
+    private double speed;
+    private double noise;
+    private double stateOnWire;
+    private Wire currentWire;
+    private boolean isAlive = true;
 }
