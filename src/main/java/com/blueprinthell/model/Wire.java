9c6a2954e818e9da4f0a94e354f09f01b22acaab
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-21 23:08:33 +0330

    Add fields to Wire class

--- src/main/java/com/blueprinthell/model/Wire.java
+++ src/main/java/com/blueprinthell/model/Wire.java
@@ -1,4 +1,11 @@
 package com.blueprinthell.model;
 
+import java.util.ArrayList;
+
 public class Wire {
+    private int id;
+    private Port sourcePort;
+    private Port destinationPort;
+    private double length;
+    private Packet packetOnWire;
 }
