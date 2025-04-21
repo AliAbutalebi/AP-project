0fb3cdd381c5a9ae89a1a474edbcebff58c3c9e8
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-22 01:53:59 +0330

    Add fields to GameState class

--- src/main/java/com/blueprinthell/model/GameState.java
+++ src/main/java/com/blueprinthell/model/GameState.java
@@ -1,4 +1,15 @@
 package com.blueprinthell.model;
 
+import java.util.ArrayList;
+
 public class GameState {
+    private ArrayList<SystemNode> systemNodes;
+    private ArrayList<Wire> wires;
+    private ArrayList<Packet> packets;
+    private int coins;
+    private int initialPackets;
+    private int packetLoss;
+    private double timeProgress;
+    private static final double LOSS_THRESHOLD = 0.5;
+    private boolean gameOver;
 }
