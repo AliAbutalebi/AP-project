e9f56e8f707cd800a4932c52c701c65783aef183
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-22 01:19:40 +0330

    Add fields to SystemNode class
    made Dimensions as comments to implement later

--- src/main/java/com/blueprinthell/model/SystemNode.java
+++ src/main/java/com/blueprinthell/model/SystemNode.java
@@ -1,4 +1,19 @@
 package com.blueprinthell.model;
 
+import javafx.geometry.Dimension2D;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SystemNode {
+    private int id;
+    private final ArrayList<Port> inputPorts = new ArrayList<>();
+    private final ArrayList<Port> outputPorts = new ArrayList<>();
+    private final Queue<Packet> packetQueue = new LinkedList<>();
+    private static final int maxPacketInQueue = 5;
+    // private final Dimension2D position;
+    // private final Dimension2D size;
+    private boolean isActive = false;
+    
 }
