8379e9f3142c5a701c240bcb83ddfe4af0a0065b
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-24 18:59:20 +0330

    Add start method to Main class

--- src/main/java/com/blueprinthell/Main.java
+++ src/main/java/com/blueprinthell/Main.java
@@ -1,4 +1,21 @@
 package com.blueprinthell;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/blueprinthell/view/MainMenu.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Blueprint Hell");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
