5c193c205a0271d43d6b276e6e596988215c2b1b
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-25 18:14:08 +0330

    Add event handlers for menu buttons

--- src/main/java/com/blueprinthell/controller/MenuController.java
+++ src/main/java/com/blueprinthell/controller/MenuController.java
@@ -1,4 +1,28 @@
 package com.blueprinthell.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class MenuController extends BaseController {
+
+    public void initialize() {}
+
+    @FXML
+    private void handleStartGame(ActionEvent event) throws IOException {
+        super.switchScene("/com/blueprinthell/view/GameView.fxml", event);
+    }
+    @FXML
+    private void handleSelectLevel(ActionEvent event) throws IOException {
+        //TODO: create "Select Level" scene and write the SwitchScene
+    }
+    @FXML
+    private void handleSettings(ActionEvent event) throws IOException {
+        //TODO: create "Settings" scene and write the SwitchScene
+    }
+    @FXML
+    private void handleExit(ActionEvent event) throws IOException {
+        System.exit(0);
+    }
 }
