d7662bc0b01abc4594d993651619b173e2f91d76
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-26 14:39:37 +0330

    Add "center" field to Packet class

--- src/main/java/com/blueprinthell/model/Packet.java
+++ src/main/java/com/blueprinthell/model/Packet.java
@@ -1,11 +1,14 @@
 package com.blueprinthell.model;
 
+import javafx.geometry.Dimension2D;
+
 public abstract class Packet {
 
     private int id;
     private double speed;
     private double noise;
     private double stateOnWire;
+    private Dimension2D center;
     private Wire currentWire;
     private boolean isAlive = true;
 }
