61c276ccc4304e656f1a0780a4f584724c975526
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-26 15:20:35 +0330

    Add packets to GameMap

--- src/main/java/com/blueprinthell/model/GameMap.java
+++ src/main/java/com/blueprinthell/model/GameMap.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 
 public class GameMap {
     private ArrayList<SystemNode> systemNodes;
+    private ArrayList<Packet> packets;
     private double maxWireLength; //TODO: double-check for making maxWireLength final
 
     public void setSystemNodes(ArrayList<SystemNode> systemNodes) {
         this.systemNodes = systemNodes;
     }
+
     public ArrayList<SystemNode> getSystemNodes() {
         return systemNodes;
     }
+
+    public void setPackets(ArrayList<Packet> packets) {
+        this.packets = packets;
+    }
+
+    public ArrayList<Packet> getPackets() {
+        return packets;
+    }
+
     public void setMaxWireLength(double maxWireLength) {
         this.maxWireLength = maxWireLength;
     }
+
     public double getMaxWireLength() {
         return maxWireLength;
     }
