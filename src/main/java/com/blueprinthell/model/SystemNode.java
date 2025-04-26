3d3209fcc200785502f03835a6d4b445ec2a438d
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-26 14:42:51 +0330

    Change start and end systems to reference system

--- src/main/java/com/blueprinthell/model/SystemNode.java
+++ src/main/java/com/blueprinthell/model/SystemNode.java
@@ -1,7 +1,5 @@
 package com.blueprinthell.model;
 
-import javafx.geometry.Dimension2D;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,20 +8,14 @@ public abstract class SystemNode {
     private int id;
     private final Queue<Packet> packetQueue = new LinkedList<>();
     private static final int maxPacketInQueue = 5;
+    private final ArrayList<Port> inputPorts = new ArrayList<>();
+    private final ArrayList<Port> outputPorts = new ArrayList<>();
     // private final Dimension2D POSITION;
     // private final Dimension2D SIZE;
     private boolean isActive = false;
 }
 
-class regularSystemNode extends SystemNode {
-    private final ArrayList<Port> inputPorts = new ArrayList<>();
-    private final ArrayList<Port> outputPorts = new ArrayList<>();
-}
+class ReferenceSystemNode extends SystemNode {
 
-class StartSystemNode extends SystemNode {
-    private final ArrayList<Port> outputPorts = new ArrayList<>();
-}
 
-class EndSystemNode extends SystemNode {
-    private final ArrayList<Port> inputPorts = new ArrayList<>();
 }
