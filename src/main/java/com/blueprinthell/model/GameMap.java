2ac094691cd532e991c689ed4b81bbb164a5f456
Ali Abutalebi <abutalebi1385@gmail.com>
2025-04-26 15:14:54 +0330

    Add GameMap class to model package

---
+++ src/main/java/com/blueprinthell/model/GameMap.java
@@ -0,0 +1,22 @@
+package com.blueprinthell.model;
+
+import java.util.ArrayList;
+
+public class GameMap {
+    private ArrayList<SystemNode> systemNodes;
+    private double maxWireLength; //TODO: double-check for making maxWireLength final
+
+    public void setSystemNodes(ArrayList<SystemNode> systemNodes) {
+        this.systemNodes = systemNodes;
+    }
+    public ArrayList<SystemNode> getSystemNodes() {
+        return systemNodes;
+    }
+    public void setMaxWireLength(double maxWireLength) {
+        this.maxWireLength = maxWireLength;
+    }
+    public double getMaxWireLength() {
+        return maxWireLength;
+    }
+
+}
