3a0b6e2060208da2efc543186ed3414691bf175a
Ali Abutalebi <abutalebi1385@gmail.com>
2025-05-01 13:54:04 +0330

    Remove updatePackets method to rewrite later

--- src/main/java/com/blueprinthell/model/GameMap.java
+++ src/main/java/com/blueprinthell/model/GameMap.java
@@ -28,49 +28,22 @@ public class GameMap {
     public void setWires(ArrayList<Wire> wires) {
         this.wires = wires;
     }
+
     public ArrayList<Wire> getWires() {
         return wires;
     }
+
     public void setPackets(ArrayList<Packet> packets) {
         this.activePackets = packets;
     }
+
     public ArrayList<Packet> getPackets() {
         return activePackets;
-    }
-
-    public void updatePackets(double deltaTime) {
-        ArrayList<Packet> toRemove = new ArrayList<>();
-
-        for (Packet packet : activePackets) {
-            if (packet.isOnWire()) {
-                double newProgress = packet.getProgressOnWire();
-                if (packet.getShapeType() == packet.getCurrentWire().getShapeType()) {
-                    newProgress = packet.getProgressOnWire() + packet.getSpeed() * deltaTime;
-                }
-                else if (packet.getShapeType() == ShapeType.SQUARE) {
-                    newProgress = packet.getProgressOnWire() + packet.getSpeed() / 2 * deltaTime;
-                }
-                else if (packet.getShapeType() == ShapeType.TRIANGLE) {
-                    newProgress = packet.getProgressOnWire() + packet.getSpeed() * deltaTime + packet.getAcceleration() * Math.pow(deltaTime, 2) / 2;
-                }
-                packet.setProgressOnWire(Math.min(newProgress, 1.0));
-                packet.setLocation(packet.getCurrentWire().interpolate(packet.getProgressOnWire()));
-            }
-
-            if (!packet.isAlive()) {
-                toRemove.add(packet);
-                // gameState.incrementPacketLoss();
-            }
-        }
-
-        activePackets.removeAll(toRemove);
     }
 
     public void resetPackets(ArrayList<Packet> snapshot) {
 
     }
-
-
 
 
 }
